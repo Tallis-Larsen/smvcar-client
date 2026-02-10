54c4aec17378494e99c9952668080a7fa54a9442
tallis-larsen <tallisblarsen@gmail.com>
2026-02-09 23:14:14 -0600

    Wifi dialogue box exists but does not have any functionality yet.

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -2,7 +2,14 @@
 #include <QWidget>
 #include <QShortcut>
 #include <QLabel>
+#include <QFileSystemWatcher>
+#include <QDir>
+#include <QFileInfo>
+#include <QSocketNotifier>
+#include <fcntl.h>
+#include <unistd.h>
 #include <NetworkManagerQt/Device>
+#include <linux/input.h>
 #include "WiFiDialogue.h"
 #include "DefaultPalette.h"
 #include "LapStopwatch.h"
@@ -13,10 +20,23 @@ public:
     MainWindow();
     void start();
 private:
-    QLabel shortcutLabel;QLabel stateIndicator;
+    QLabel shortcutLabel;
+    QLabel wifiStateIndicator;
+    QLabel clickerStateIndicator;
     LapStopwatch stopwatch;
     WiFiDialogue wifiDialogue;
     NetworkManager::WirelessDevice::Ptr wlan0;
+    QFileSystemWatcher clickerWatcher;
+    QFile* clicker = nullptr;
+    bool clickerConnected = false;
+    QSocketNotifier* clickerNotifier = nullptr;
+    bool waitingForSwipe = false;
+
+    void connectToClicker();
+    void setClickerConnected(bool isConnected);
+    void disconnectClicker();
 private slots:
     void wifiStateChanged(NetworkManager::Device::State newState, NetworkManager::Device::State oldState);
+    void checkClickerConnected(const QString& path);
+    void recieveClickerInput();
 };
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,7 +1,8 @@
 #include "../include/MainWindow.h"
 
 MainWindow::MainWindow() : stopwatch(this), wifiDialogue(this),
-    shortcutLabel("WiFi: CTRL + W", this), stateIndicator(this) {
+    shortcutLabel("WiFi: CTRL + W", this), wifiStateIndicator(this),
+    clickerStateIndicator(this), clickerWatcher(this) {
 
     initDefaultPalette();
     resize(480, 320);
@@ -9,8 +10,9 @@ MainWindow::MainWindow() : stopwatch(this), wifiDialogue(this),
     setAutoFillBackground(true);
 
     shortcutLabel.setGeometry(0, 0, 125, 25);
-    stateIndicator.setGeometry(200, 0, 280, 25);
-    stateIndicator.setAlignment(Qt::AlignRight);
+    wifiStateIndicator.setGeometry(280, 0, 200, 25);
+    wifiStateIndicator.setAlignment(Qt::AlignRight);
+    clickerStateIndicator.setGeometry(150, 0, 200, 25);
 
     // ReSharper disable once CppDFAMemoryLeak | This does not actually leak because Qt handles the delete
     QShortcut *wifiShortcut = new QShortcut(QKeySequence("Ctrl+W"), this); // Creating a shortcut
@@ -28,10 +30,17 @@ MainWindow::MainWindow() : stopwatch(this), wifiDialogue(this),
         qFatal("Could not find network interface");
     }
 
-    // Create callback to update state indicator
+    // Create a callback to update the state indicator
     connect(wlan0.data(), &NetworkManager::Device::stateChanged, this, &MainWindow::wifiStateChanged);
     // Initially call function to ensure the state doesn't say "Disconnected" when it actually connected before the program started
     wifiStateChanged(wlan0->state(), wlan0->state()); // oldState argument doesn't matter, it's just required for the signal to be accepted
+
+    // Watching the kernel for new bluetooth devices
+    clickerWatcher.addPath("/dev/input/");
+    connect(&clickerWatcher, &QFileSystemWatcher::directoryChanged, this, &MainWindow::checkClickerConnected);
+
+    // Call the function once to check if the clicker connected before the program started
+    checkClickerConnected("/dev/input/");
 
 }
 
@@ -44,7 +53,7 @@ void MainWindow::wifiStateChanged(NetworkManager::Device::State newState, Networ
     Q_UNUSED(oldState)
 
     // Copy palette so it can be modified
-    QPalette palette = stateIndicator.palette();
+    QPalette palette = wifiStateIndicator.palette();
 
     // Cool C++ feature I learned: case fallthrough. Turns out you can do this and it works.
     switch (newState) {
@@ -54,7 +63,7 @@ void MainWindow::wifiStateChanged(NetworkManager::Device::State newState, Networ
         case NetworkManager::Device::Disconnected:
         case NetworkManager::Device::Failed:
         case NetworkManager::Device::Deactivating:
-            stateIndicator.setText("Disconnected");
+            wifiStateIndicator.setText("Disconnected");
             palette.setColor(QPalette::WindowText, QColor(255, 0, 0)); // Red
             break;
         case NetworkManager::Device::Preparing:
@@ -63,16 +72,186 @@ void MainWindow::wifiStateChanged(NetworkManager::Device::State newState, Networ
         case NetworkManager::Device::NeedAuth:
         case NetworkManager::Device::CheckingIp:
         case NetworkManager::Device::WaitingForSecondaries:
-            stateIndicator.setText("Connecting");
+            wifiStateIndicator.setText("Connecting");
             palette.setColor(QPalette::WindowText, QColor(255, 255, 0)); // Yellow
             break;
         case NetworkManager::Device::Activated:
-            stateIndicator.setText("Connected");
+            wifiStateIndicator.setText("Connected");
             palette.setColor(QPalette::WindowText, QColor(0, 255, 0)); // Green
             break;
     }
 
     // Re-apply the palette
-    stateIndicator.setPalette(palette);
+    wifiStateIndicator.setPalette(palette);
 
+}
+
+// This function could look prettier, but for now it works.
+void MainWindow::checkClickerConnected(const QString& path) {
+    qDebug() << "checkClickerConnected called";
+    QDir deviceDirectory(path);
+    QStringList filters("event*");
+    deviceDirectory.setNameFilters(filters);
+
+    // Iterate through the files in the directory and check for a device named "JX-03"
+    for (const QString& file : deviceDirectory.entryList(QDir::System)) {
+        QFile device(deviceDirectory.absoluteFilePath(file));
+
+        // Open the device and skip if it fails
+        if (!device.open(QIODevice::ReadOnly)) { continue; }
+
+        // Creates a buffer to store the name in for reading. The '0' means it will initialize the buffer with '0's to start.
+        QByteArray nameBuffer(256, 0);
+
+        // Grab the name and check to make sure it's not empty
+        if (ioctl(device.handle(), EVIOCGNAME(nameBuffer.size()), nameBuffer.data()) >= 0) {
+            // Immediately converting the raw buffer to a QString
+            QString deviceName = QString::fromLocal8Bit(nameBuffer.constData());
+
+            if (deviceName.contains("JX-03", Qt::CaseInsensitive)) {
+
+                // Close now so that we don't open twice during connectToClicker()
+                device.close();
+
+                if (!clickerConnected) {
+                    // Just in case there's an existing clicker object
+                    disconnectClicker();
+                    // Don't need to worry about delete because it will die with the program
+                    clicker = new QFile(deviceDirectory.absoluteFilePath(file));
+                    connectToClicker();
+                }
+
+                return;
+            }
+        }
+    }
+
+    setClickerConnected(false);
+
+}
+
+void MainWindow::connectToClicker() {
+    qDebug() << "connectToClicker called";
+
+    // Just in case the clicker is null
+    if (!clicker) { return; }
+
+    // Open the file and disconnect if it fails (new QFile() doesn't open automatically like stack allocation does)
+    if (!clicker->open(QIODevice::ReadOnly)) {
+        disconnectClicker();
+        return;
+    }
+
+    // Get the current file flags
+    int flags = fcntl(clicker->handle(), F_GETFL, 0);
+    // Add a new flag that enables non-blocking mode so that we can call read() without waiting for data
+    fcntl(clicker->handle(), F_SETFL, flags | O_NONBLOCK);
+
+    // Grab (intercept) the bluetooth signals and disconnect if it fails
+    if (ioctl(clicker->handle(), EVIOCGRAB, 1) < 0) {
+        disconnectClicker();
+        return;
+    }
+
+    // Delete the notifier if it already exists
+    if (clickerNotifier) {
+        clickerNotifier->setEnabled(false);
+        delete clickerNotifier;
+        clickerNotifier = nullptr;
+    }
+
+    // Create new notifier
+    clickerNotifier = new QSocketNotifier(clicker->handle(), QSocketNotifier::Read, this);
+    // Create a callback
+    connect(clickerNotifier, &QSocketNotifier::activated, this, &MainWindow::recieveClickerInput);
+
+    setClickerConnected(true);
+
+}
+
+void MainWindow::recieveClickerInput() {
+    qDebug() << "entering recieveClickerInput";
+
+    // Disable notifier to make sure it can't call this function again while it's running
+    clickerNotifier->setEnabled(false);
+
+    struct input_event inputEvent;
+
+    int bytesRead;
+
+    // Loop through all events in the buffer
+    while (( bytesRead = read(clicker->handle(), &inputEvent, sizeof(inputEvent))) > 0) {
+
+        qDebug() << "receiveClickerInput loop ran";
+
+        // One of the buttons was pressed, now we wait for the next event for the direction
+        if (inputEvent.type == EV_KEY && inputEvent.code == 330 && inputEvent.value == 1) {
+            waitingForSwipe = true;
+        }
+
+        if (waitingForSwipe && inputEvent.type == EV_ABS && inputEvent.code == 1) {
+
+            // If the value is below 1600, the top button was pressed. Otherwise, the bottom button was pressed.
+            if (inputEvent.value < 1600) {
+                qDebug("TOP PRESSED");
+            } else {
+                qDebug("BOTTOM PRESSED");
+            }
+
+            waitingForSwipe = false;
+
+        }
+
+    }
+
+    qDebug() << "Exiting recieveClickerInput";
+
+    // Re-enable notifier
+    clickerNotifier->setEnabled(true);
+
+    // We assume a critical error while reading the file means the device disconnected, so this filters out non-critical errors.
+    if (bytesRead < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
+        disconnectClicker();
+    }
+
+}
+
+void MainWindow::setClickerConnected(bool isConnected) {
+
+    clickerConnected = isConnected;
+
+    qDebug() << "clickerConnected set to:" << clickerConnected;
+
+    QPalette palette = clickerStateIndicator.palette();
+
+    if (isConnected) {
+        clickerStateIndicator.setText("Connected");
+        palette.setColor(QPalette::WindowText, QColor(0, 255, 0)); // Green
+    } else {
+        clickerStateIndicator.setText("Disconnected");
+        palette.setColor(QPalette::WindowText, QColor(255, 0, 0));
+    }
+
+    clickerStateIndicator.setPalette(palette);
+
+}
+
+void MainWindow::disconnectClicker() {
+    qDebug() << "disconnectClicker called";
+    if (clickerNotifier) {
+        clickerNotifier->setEnabled(false);
+        delete clickerNotifier;
+        clickerNotifier = nullptr;
+    }
+    if (clicker) {
+        qDebug() << "Clicker Disconnected";
+        if (clicker->isOpen()) {
+            // Release the grab
+            ioctl(clicker->handle(), EVIOCGRAB, 0);
+            clicker->close();
+        }
+        delete clicker;
+        clicker = nullptr;
+    }
+    setClickerConnected(false);
 }
